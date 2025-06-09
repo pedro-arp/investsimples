1ee78695bf69e778126b94b5dcde26e7f05a5d25
pedro-arp <pedroagtrodrigues@gmail.com>
2025-06-09 18:40:44 -0300

    Initial commit

---
+++ src/main/java/com/investsimples/InvestsimplesApplication.java
@@ -0,0 +1,13 @@
+package com.investsimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestsimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestsimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/investsimples/TestInvestsimplesApplication.java
@@ -0,0 +1,11 @@
+package com.investsimples;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestInvestsimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(InvestsimplesApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
